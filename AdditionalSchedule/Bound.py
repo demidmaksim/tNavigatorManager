a0469f76072dff76102aa1e8d8c8798828e1184a
demid.maksim <demid.maksim@gmail.com>
2021-02-14 13:53:45 +0300

    create bound class

--- AdditionalSchedule/Bound.py
+++ AdditionalSchedule/Bound.py
@@ -2,42 +2,44 @@ import pandas as pd
 
 
 class DebitLimits:
+
+    well = 'well'
+    valve = 'valve/segment'
+    date = 'date'
+    min_liq = 'min liquid'
+    max_liq = 'max liquid'
+    fluid = 'fluid'
+
     def __init__(self):
         self.df = None
 
     def get_limit(self, date, well, segment):
 
-        pattern1 = self.df['well'] == well
+        pattern1 = self.df[DebitLimits.well] == well
 
         if segment is not None:
-            pattern2 = self.df['valve/segment'] == segment
+            pattern2 = self.df[DebitLimits.valve] == segment
         else:
-            pattern2 = self.df['valve/segment'].isna()
+            pattern2 = self.df[DebitLimits.valve].isna()
 
-        pattern3 = self.df['date'] <= date
+        pattern3 = self.df[DebitLimits.date] <= date
         pattern = pattern1 & pattern2 & pattern3
 
         new_df = self.df[pattern]
 
         if new_df.shape[0] != 0:
-            new_df.loc[new_df.shape[0], 'date'] = date
-            new_df.sort_values('date')
+            new_df.loc[new_df.shape[0], DebitLimits.date] = date
+            new_df.sort_values(DebitLimits.date)
             new_df = new_df.interpolate(method='pad')
-            pattern = new_df['date'] == date
-            pattern = pd.DataFrame(pattern)
-
-            if pattern.shape[0] > 1:
-                return new_df[pattern]
-            else:
-                return new_df[pattern][0]
+            return new_df.iloc[len(new_df)-1]
 
         else:
-            new_df.loc[new_df.shape[1] - 1, 'date'] = date
-            new_df['well'] = well
-            new_df['valve/segment'] = segment
-            new_df['min liquid'] = 0
-            new_df['max liquid'] = float('inf')
-            new_df['fluid'] = 'FLUID'
+            new_df.loc[new_df.shape[1] - 1, DebitLimits.date] = date
+            new_df[DebitLimits.well] = well
+            new_df[DebitLimits.valve] = segment
+            new_df[DebitLimits.min_liq] = 0
+            new_df[DebitLimits.max_liq] = float('inf')
+            new_df[DebitLimits.fluid] = 'FLUID'
             return new_df
 
 
@@ -52,6 +54,6 @@ class DebitLimitsConstructor:
 
     @staticmethod
     def prepare_date(df) -> pd.DataFrame:
-        dates = pd.to_datetime(df['date'])
-        df['date'] = dates
+        dates = pd.to_datetime(df[DebitLimits.date])
+        df[DebitLimits.date] = dates
         return df
